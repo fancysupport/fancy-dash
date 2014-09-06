5a0fe31ce2a38b9a519e7ebad7c9dd6c13ce8dc9
niftykins <niftykins@gmail.com>
2014-09-07 00:22:51 +0800

    create line widgets

--- src/js/dash.js
+++ src/js/dash.js
@@ -1,11 +1,15 @@
 var Dash = {
 	url: 'http://api.fancysupport.com:4000/public',
 	active: null,
+	widgets: {},
 	app: null,
 
 	init: function() {
 		// cache
 		this.app = this.id('app');
+
+		Chart.defaults.global.showTooltips = false;
+		Chart.defaults.global.rotateXLabels = false;
 
 		this.hash_changed();
 
@@ -19,6 +23,15 @@ var Dash = {
 
 		if (token) this.get_dash(token);
 		else this.render_empty();
+	},
+
+	entries: function(map) {
+		var e = [];
+		for (var key in map) e.push({
+			key: key,
+			value: map[key]
+		});
+		return e;
 	},
 
 	id: function(q) {
@@ -35,6 +48,84 @@ var Dash = {
 		}
 	},
 
+	generate_timeseries: function(data, points, period) {
+		var end = {};
+		var now = new Date().getTime();
+
+		for (var i=0; i<points; i++) {
+			end[now-i*period] = 0;
+		}
+
+		for (orig in data) {
+			for (time in end) {
+				if (orig*1000 >= time && Math.abs(orig*1000-time) < period) {
+					end[time] = data[orig];
+					break;
+				}
+			}
+		}
+
+		return this.entries(end);
+	},
+
+	generate_legend: function(parent, data) {
+		parent.className = 'legend';
+		var datas = data.hasOwnProperty('datasets') ? data.datasets : data;
+
+		// remove possible children of the parent
+		while(parent.hasChildNodes()) {
+			parent.removeChild(parent.lastChild);
+		}
+
+		datas.forEach(function(d) {
+			var title = document.createElement('span');
+			title.className = 'title';
+			title.style.borderColor = d.hasOwnProperty('strokeColor') ? d.strokeColor : d.color;
+			title.style.borderStyle = 'solid';
+			parent.appendChild(title);
+
+			var text = document.createTextNode(d.label);
+			title.appendChild(text);
+		});
+	},
+
+	generate_line: function(points, ctx, legend) {
+		var that = this;
+
+		// do hourly over last day for now
+		// TODO make proper time range
+		points = this.generate_timeseries(points, 24+1, 60*60*1000);
+		points.sort(function(a,b){return a.key-b.key;});
+
+		var data = {
+			labels: points.map(function(e, i) {
+				return that.timeago(parseInt(e.key, 10)/1000);
+			}),
+
+			datasets: [{
+				label: 'todo labels',
+				fillColor: "rgba(169,169,169,0.4)",
+				strokeColor: "rgba(169,169,169,1)",
+				pointColor: "rgba(169,169,169,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(169,169,169,1)",
+
+				data: points.map(function(e) { return e.value; })
+			}]
+		};
+
+		var options = {
+			scaleVerticalGridLines:false,
+			skipXLabels: 6,
+			bezierCurve: false
+		};
+
+		this.generate_legend(legend, data);
+
+		return new Chart(ctx).Line(data, options);
+	},
+
 	get_dash: function(token) {
 		var that = this;
 
@@ -46,7 +137,7 @@ var Dash = {
 
 			if (ok)	{
 				that.active = ok.data;
-				that.render_dash();
+				that.init_dash();
 			}
 
 			if (err && err.code === 404)
@@ -54,26 +145,44 @@ var Dash = {
 		});
 	},
 
-	render_notfound: function() {
-		this.app.innerHTML = Templates.notfound();
-	},
+	init_dash: function() {
+		this.render_dash();
 
-	render_empty: function() {
-		this.app.innerHTML = Templates.empty();
-	},
+		var d = this.active;
 
-	render_dash: function() {
-		this.app.innerHTML = Templates.dash(this.active);
-
-		for (var i=0; i<this.active.widgets.length; i++) {
-			this.render_widget(this.active.widgets[i]);
+		for (var i=0; i<d.widgets.length; i++) {
+			this.init_widget(d.widgets[i]);
 		}
 	},
 
-	render_widget: function(widget) {
-		var w = document.createElement('div');
-		w.innerHTML = Templates.widget(widget);
-		this.id('dash').appendChild(w.firstChild);
+	init_widget: function(w) {
+		var that = this;
+
+		// assign to list to keep track easier
+		this.widgets[w.name] = w;
+
+		this.render_widget(w);
+		
+		var node = this.qs('[data-id='+w.name+']');
+
+		if (w.type === 'line') {
+			var ctx = node.querySelector('canvas').getContext('2d');
+			var legend = node.querySelector('.legend');
+
+			this.get_widget_data(w.name, function(ok, err) {
+				console.log('widget data', ok, err);
+				if (ok) {
+					w.chart = that.generate_line(ok.data[0].dps, ctx, legend);
+				}
+			});
+		}
+	},
+
+	get_widget_data: function(name, cb) {
+		this.ajax({
+			method: 'GET',
+			url: '/dashboards/' + this.active.token + '/data/' + name
+		}, cb);
 	},
 
 	ajax: function (opts, cb) {
@@ -105,6 +214,54 @@ var Dash = {
 			}
 		};
 		request.send();
+	},
+
+	render_notfound: function() {
+		this.app.innerHTML = Templates.notfound();
+	},
+
+	render_empty: function() {
+		this.app.innerHTML = Templates.empty();
+	},
+
+	render_dash: function() {
+		this.app.innerHTML = Templates.dash(this.active);
+	},
+
+	render_widget: function(widget) {
+		// unsure how to just append a string to DOM
+		// so take the DOM from an element and append that
+		var w = document.createElement('div');
+		w.innerHTML = Templates.widget(widget);
+		this.id('dash').appendChild(w.firstChild);
+	},
+
+	timeago: function(time) {
+		var
+		local = new Date().getTime()/1000,
+		offset = Math.abs((local - time)),
+		span   = [],
+		MINUTE = 60,
+		HOUR   = 3600,
+		DAY    = 86400,
+		WEEK   = 604800,
+		MONTH  = 2629744,
+		YEAR   = 31556926;
+		DECADE = 315569260;
+
+		if (offset <= MINUTE)              span = [ '', 'moments' ];
+		else if (offset < (MINUTE * 60))   span = [ Math.round(Math.abs(offset / MINUTE)), 'min' ];
+		else if (offset < (HOUR * 24))     span = [ Math.round(Math.abs(offset / HOUR)), 'hr' ];
+		else if (offset < (DAY * 7))       span = [ Math.round(Math.abs(offset / DAY)), 'day' ];
+		else if (offset < (WEEK * 52))     span = [ Math.round(Math.abs(offset / WEEK)), 'week' ];
+		else if (offset < (YEAR * 10))     span = [ Math.round(Math.abs(offset / YEAR)), 'year' ];
+		else if (offset < (DECADE * 100))  span = [ Math.round(Math.abs(offset / DECADE)), 'decade' ];
+		else                               span = [ '', 'a long time' ];
+
+		span[1] += (span[0] === 0 || span[0] > 1) ? 's' : '';
+		span = span.join(' ');
+
+		return (time <= local)  ? span + ' ago' : 'in ' + span;
 	}
 };
 
