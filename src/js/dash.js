7c127d854df506d4ade1b4036a205d9fa759b53b
niftykins <niftykins@gmail.com>
2014-09-13 09:48:54 +0800

    line graph done

--- src/js/dash.js
+++ src/js/dash.js
@@ -91,11 +91,171 @@ var Dash = {
 		var that = this;
 		var format = d3.format('.4s');
 
-		this.get_widget_data(widget.id, function(ok, err) {
-			if (ok && ok.data) {
-				if (ok.data.length === 0) ok.data[0] = {dps:{}};
-			}
-		})
+		var period = 60*60*1000;
+		var points = 25;
+		var colours = [
+			'#FF9F29',
+			'#FF742E',
+			'#F55332'
+		];
+
+		var margin = {top: 20, right: 10, bottom: 35, left: 40};
+		var width = widget.size[0] * 200 - 20 - margin.left - margin.right;
+		var height = widget.size[1] * 200 - 20 - margin.top - margin.bottom;
+
+		var x = d3.scale.linear()
+			.range([0, width]);
+
+		var y = d3.scale.linear()
+			.range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+			.scale(x)
+			.tickSize(0, 0, 0)
+			.tickPadding(12)
+			.tickFormat(function(d) { return that.timeago(d/1000); })
+			.orient('bottom');
+
+		var yAxis = d3.svg.axis()
+			.scale(y)
+			.ticks(5)
+			.orient('left')
+			.tickSize(-width, -width, 0)
+			.tickPadding(8)
+			.tickFormat(function(d) {
+				var p = d3.formatPrefix(d);
+				return p.scale(d) + p.symbol;
+			});
+
+		var node = d3.select('[data-id=' + widget.name + ']')
+		var svg = node.append('svg')
+				.attr('width', width + margin.left + margin.right)
+				.attr('height', height + margin.top + margin.bottom)
+			.append('g')
+				.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+		var stack = d3.layout.stack().values(function(d) {
+			return d.values;
+		});
+
+		line = d3.svg.line()
+			.interpolate('linear')
+			.x(function(d) { return x(d.x); })
+			.y(function(d) { return y(d.y); });
+
+		svg.append('g')
+			.attr('class', 'x axis')
+			.attr('transform', 'translate(0,' + height + ')');
+
+		svg.append('g')
+			.attr('class', 'y axis')
+
+		var tip = d3.tip()
+			.attr('class', 'd3-tip');
+
+		svg.call(tip);
+
+		function draw() {
+			that.get_widget_data(widget.id, function(ok, err) {
+				if (ok && ok.data) {
+					console.log('new data');
+					var sources = [];
+					for (var i=0; i<ok.data.length; i++) {
+						for (var j=0; j<widget.sources.length; j++) {
+							if (ok.data[i].id === widget.sources[j].id && widget.sources[j].source === 'tsdb') {
+								ok.data[i].name = widget.sources[j].name;
+								sources.push(ok.data[i]);
+							}
+						}
+					}
+
+					if (sources.length === 0) return;
+
+					var data = that.generate_layered_series(sources, points, period);
+					for (var i=0; i<data.length; i++) {
+						data[i].colour = colours[i];
+					}
+					var layeredData = stack(data);
+
+					// an extra period at either end for padding?
+					x.domain([Date.now()-points*period+period, Date.now()+period]);
+
+					// make domain slightly larger than the actual data
+					y.domain([0, d3.max(layeredData, function(layer) {
+						return d3.max(layer.values, function(d) {
+							return d.y;
+						}) * 1.1;
+					})]);
+
+					xAxis.tickValues(data[0].values.map(function(d) { return d.x; }));
+					svg.select('.x.axis').call(xAxis);
+					svg.select('.y.axis').call(yAxis);
+
+					// remove most of the ticks
+					svg.selectAll('.x.axis > .tick')
+						.each(function(d, i) {
+							if (width < 200) {
+								if (i !== 0 && i !== points-1)
+									this.remove();
+								return;
+							}
+
+							if (i % Math.floor(points/4) !== 0)
+								this.remove();
+						});
+
+					tip.html(function(d, e) {
+						var s = that.timeago(+d.x/1000)
+						for (var i=0; i<layeredData.length; i++) {
+							s += '<br/>' + '<span class="value">' + format(layeredData[i].values[e].y);
+							s += '<span class="key" style="background-color:' + layeredData[i].colour + ';">';
+							s += '</span></span>';
+						}
+						return s;
+					});
+
+					svg.selectAll('.layer').remove(); // clear out the old
+					var layers = svg.selectAll('.layer')
+						.data(layeredData)
+					.enter().append('g')
+						.attr('class', 'layer');
+
+					layers.selectAll('path').remove();
+					layers.append('path')
+						.attr('class', 'line')
+						.attr('d', function(d) { return line(d.values); })
+						.style("stroke", function(d, i) { return layeredData[i].colour; });
+
+					var circle = layers.selectAll('circle').data(function(d) {
+						return d.values;
+					});
+
+					circle.enter().append('circle')
+						.attr('class', 'circle')
+						.attr('cx', function(d) { return x(d.x); })
+						.attr('cy', function(d) { return y(d.y); })
+						.attr('r', 5)
+						.style('fill', function(d, e, i) { return layeredData[i].colour; })
+						.on('mouseover', tip.show)
+						.on('mouseout', tip.hide);
+
+					var legend = {};
+					legend.total = d3.sum(layeredData, function(layer, i) {
+						var t = d3.sum(layer.values, function(d) {
+							return d.y;
+						});
+						layer.total = t;
+						return t;
+					});
+					legend.layers = layeredData;
+
+					that.generate_legend(node, legend);
+				}
+			});
+		}
+
+		draw();
+		setInterval(draw, 60*1000);
 	},
 
 	generate_bar: function(widget) {
@@ -164,7 +324,7 @@ var Dash = {
 		function draw() {
 			that.get_widget_data(widget.id, function(ok, err) {
 				if (ok && ok.data) {
-					console.log('new data', ok.data);
+					console.log('new data');
 					var sources = [];
 					for (var i=0; i<ok.data.length; i++) {
 						for (var j=0; j<widget.sources.length; j++) {
