ddb300de48003907a0958a0472e1e66a408b106f
niftykins <niftykins@gmail.com>
2014-10-25 17:14:19 +0800

    fix counter

--- src/js/widgets/counter.js
+++ src/js/widgets/counter.js
@@ -9,26 +9,12 @@ Dash.generate_counter = function(widget) {
 		that.get_widget_data(widget, function(ok, err) {
 			if (ok && ok.data) {
 				console.log('new data counter', ok.data);
-				var sources = [];
-				for (var i=0; i<ok.data.length; i++) {
-					for (var j=0; j<widget.sources.length; j++) {
-						if (ok.data[i].id === widget.sources[j].id && widget.sources[j].source === 'internal') {
-							var key = Object.keys(ok.data[i].data)[0];
-							ok.data[i].data = ok.data[i].data[key];
-							ok.data[i].colour = widget.sources[j].config.colour;
-							ok.data[i].name = widget.sources[j].name;
 
-							sources.push(ok.data[i]);
-						}
-					}
-				}
-
-				if (sources.length === 0) return;
-
+				// only do one source
 				counter.html(Templates.counter({
-					amount: sources[0].data,
-					colour: sources[0].colour,
-					name: sources[0].name,
+					amount: parseFloat(ok.data[0].data[0].results[0].values[0][1]) || 0,
+					colour: widget.sources[0].config.colour,
+					name: ok.data[0].data[0].results[0].name,
 					size: widget.size
 				}));
 			}
