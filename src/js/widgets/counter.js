41f353cce57bc1098e70fd5652808c9520c6e7ca
niftykins <niftykins@gmail.com>
2014-10-31 22:04:19 +0800

    fix text size

--- src/js/widgets/counter.js
+++ src/js/widgets/counter.js
@@ -9,12 +9,24 @@ Dash.generate_counter = function(widget) {
 			if (ok && ok.data) {
 				console.log('new data counter', ok.data);
 
-				// only do one source
+				var big = 25 * widget.size[0] * 2;
+				var small = 12.5 * widget.size[0] * 2;
+
+				if (widget.size[0] > widget.size[1]) {
+					big *= 0.8;
+					small *= 0.8;
+				}
+
+				if (big > widget.size[1]*200/2) {
+					big = widget.size[1]*200 * 0.4;
+					small = big * 0.6;
+				}
+
 				counter.html(Templates.counter({
 					amount: parseFloat(ok.data[0].values[0][1]) || 0,
 					colour: widget.sources[0].config.colour,
 					name: widget.sources[0].name,
-					size: widget.size
+					size: [big, small]
 				}));
 			}
 		});
